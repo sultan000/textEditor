7cc943a8a94181e69c95e8fc88a47eaf15d9808f
Sultan Alshahrani <sultanalshahrani@Sultans-MacBook-Pro.local>
2019-03-07 16:42:37 +0300

    sultan

--- src/App.js
+++ src/App.js
@@ -8,13 +8,32 @@ const styles = {
 };
 
 class App extends Component {
+  state = {
+    color: "black",
+    bold: false,
+    italic: false,
+    underline: false
+  };
+  changecolor = newColor => {
+    this.setState({ color: newColor });
+  };
+
+  changestyle = style => {
+    let newstyle = !this.state[style];
+    this.setState({ [style]: newstyle });
+  };
+
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = styleNames.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          style={styles[style]}
+          key={style}
+          onClick={() => this.changestyle(style)}
+        >
           {style}
         </button>
       );
@@ -25,14 +44,24 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.changecolor(color)}
         />
       );
     });
 
+    // const color = colors[Math.floor(Math.random() * colors.length)];
+
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.color,
+            fontWeight: this.state.bold ? "bold" : "",
+            fontStyle: this.state.italic ? "italic" : "",
+            textDecorationLine: this.state.underline ? "underline" : ""
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
